c2fc8b2505529d460d1766017208c7c488e2572a
4uva <41539104+4uva@users.noreply.github.com>
2018-12-20 01:52:39 +0200

    Added empty classes and requirements

---
+++ Departures.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Departure
+{
+	public Departure()
+    {//view of the airline flight information about arrivals and departures
+     // date and time, flight number, city/ port of
+     //arrival(departure),      
+     // terminal, flight status(check-in, gate closed,
+     //arrived, departed at, unknown, canceled, expected at, delayed, in flight,gate)  
+
+        int DateandTime = new DateandTime();
+        int FlightNumber = new FlightNumber();
+        string City = new City();
+        char Terminal = new Terminal();
+        int Gate = new Gate();
+        string FlightStatus = new FlightStatus();
+   }
+}
