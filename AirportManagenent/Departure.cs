724b4d53a21d469eb3ecd7986f1356afbf263a1c
4uva <41539104+4uva@users.noreply.github.com>
2018-12-21 02:04:56 +0200

    Added empty Airport class, corrected Departure fields

---
+++ AirportManagenent/Departure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Departure
+{
+
+    //view of the airline flight information about arrivals and departures
+    // date and time, flight number, city/ port of
+    //arrival(departure),      
+    // terminal, flight status(check-in, gate closed,
+    //arrived, departed at, unknown, canceled, expected at, delayed, in flight,gate)  
+
+    readonly DateTimeOffset ScheduledDateandTime;
+    DateTimeOffset RealDateandTime;
+    readonly string FlightNumber;
+    readonly string PlanedAirport;
+    string RealDestinationAirport;
+    readonly string AssignedTerminal;
+    string RealTerminal;
+    readonly string AssignedGate;
+    string RealGate;
+    string FlightStatus; 
+
+       
+    
+}
