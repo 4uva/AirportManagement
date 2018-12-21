aafcfb53cdc440fcb17c2567dfce710aadec5f02
4uva <41539104+4uva@users.noreply.github.com>
2018-12-22 01:55:04 +0200

    Added FlightStatus enum (empty)

--- AirportManagenent/Departure.cs
+++ AirportManagenent/Departure.cs
@@ -1,25 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Departure
+namespace AirportManagenent
 {
 
-    //view of the airline flight information about arrivals and departures
-    // date and time, flight number, city/ port of
-    //arrival(departure),      
-    // terminal, flight status(check-in, gate closed,
-    //arrived, departed at, unknown, canceled, expected at, delayed, in flight,gate)  
+    public class Departure
+    {
 
-    readonly DateTimeOffset ScheduledDateandTime;
-    DateTimeOffset RealDateandTime;
-    readonly string FlightNumber;
-    readonly Airport PlanedAirport;
-    Airport RealDestinationAirport;  
-    readonly Terminal AssignedTerminal;
-    Terminal RealTerminal;
-    readonly Gate AssignedGate;
-    Gate RealGate;
-    enum FlightStatus; 
+        //view of the airline flight information about arrivals and departures
+        // date and time, flight number, city/ port of
+        //arrival(departure),      
+        // terminal, flight status(check-in, gate closed,
+        //arrived, departed at, unknown, canceled, expected at, delayed, in flight,gate)  
 
-       
-    
+        readonly DateTimeOffset ScheduledDateandTime;
+        DateTimeOffset RealDateandTime;
+        readonly string FlightNumber;
+        readonly Airport PlanedAirport;
+        Airport RealDestinationAirport;
+        readonly Terminal AssignedTerminal;
+        Terminal RealTerminal;
+        readonly Gate AssignedGate;
+        Gate RealGate;
+         FlightStatus { };
+
+
+
+    }
 }
+
