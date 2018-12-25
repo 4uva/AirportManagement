da67cebc41926b8017a08ccbf43da871590dce2e
4uva <41539104+4uva@users.noreply.github.com>
2018-12-26 00:28:56 +0200

    Temporary hardcoding the list of airports (work in progress)

--- AirportManagement/Data/All.cs
+++ AirportManagement/Data/All.cs
@@ -8,6 +8,14 @@ namespace AirportManagement.Data
     class All
     {
       public  List<Airport> Airports { get; set; }
-      
+
+        static void Create()
+        {
+            var Airports = new List<Airport>()
+            {
+             // Airport a =new 
+            };
+            // IList<int> intList = new List<int>();
+        }
     }
 }
