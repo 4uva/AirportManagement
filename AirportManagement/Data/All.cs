4452cf36e056b4f47454cd880ded8756d7c6e4cc
4uva <41539104+4uva@users.noreply.github.com>
2018-12-30 02:04:11 +0200

    Removed obsolete variants, linked names to the rest of locations, changed All's visibility to public

--- AirportManagement/Data/All.cs
+++ AirportManagement/Data/All.cs
@@ -5,31 +5,18 @@ using System.Text;
 namespace AirportManagement.Data
 {  //будущая бз добавить списки
    //аэропортов, полётов, и авиакомпаний
-    class All
+    public class All
     {
-      public  List<Airport> Airports { get; set; }
-
-        static void Create()
+        void Create()
         {
-            var airports0 = new List<Airport>()
-            {
-                new Airport(),
-                new Airport(),
-                new Airport(),
-                new Airport()
-            };
-
-            //---------------------------------------
-
             var a = new Airport();
-            //1)мы присвоили переменной а 
-            //значение нового аэропорта
+            //1)мы присвоили переменной а значение нового аэропорта
             var b = new Airport();
             var c = new Airport();
             var d = new Airport();
+
             var f = new Location();
-            //2)мы присвоили переменной f 
-            //значение локация
+            //2)мы присвоили переменной f значение локация
             var e = new Location();
             var g = new Location();
             var i = new Location();
@@ -37,38 +24,49 @@ namespace AirportManagement.Data
             //мы присвоили полю                                                                         Аэропорта 
             //значение объекта типа Location, у которого есть поле `Name`
             a.Location = f;
-            
+            b.Location = e;
+            c.Location = g;
+            d.Location = i;
+
             f.Name = "Florence";
-            //3)мы обратились к полю Name
-            //класса Location 
-           
+            //3)мы обратились к полю Name класса Location 
             e.Name = "Elabuga";
-            
             g.Name = "Gatwick";
-            
             i.Name = "Insbruk";
-
-
-
 
             var airports = new List<Airport>()
             {
-              a, b, c, d//new Airport (),new Airport(), new Airport ()
+                a, b, c, d
+                //new Airport (), new Airport (), new Airport(), new Airport (r              a.Airport="Florence";gj
             };
+         Airports = new List<Airport> { }
 
-            //---------------------------------------
 
-            var airports2 = new List<Airport>();
-            airports2.Add(new Airport());
-            airports2.Add(new Airport());
-            airports2.Add(new Airport());
-            airports2.Add(new Airport());
-
-            //---------------------------------------
-
-            var airports3 = new List<Airport>();//список на входе
-            airports3.AddRange(new List<Airport>() { new Airport(), new Airport(), new Airport(), new Airport() });
-            airports3.AddRange(new Airport[] { new Airport(), new Airport(), new Airport(), new Airport() });//массив на выходе
         }
+
+        public List<Airport> Airports { get; set; }
+        
+
     }
 }
+
+//var airports0 = new List<Airport>()
+//{
+//    new Airport(),
+//    new Airport(),
+//    new Airport(),
+//    new Airport()
+//};
+//---------------------------------------
+
+//var airports2 = new List<Airport>();
+//airports2.Add(new Airport());//в
+//airports2.Add(new Airport());
+//airports2.Add(new Airport());
+//airports2.Add(new Airport());
+
+////---------------------------------------
+
+//var airports3 = new List<Airport>();//список на входе
+//airports3.AddRange(new List<Airport>() { new Airport(), new Airport(), new Airport(), new Airport() });
+//airports3.AddRange(new Airport[] { new Airport(), new Airport(), new Airport(), new Airport() });//массив на выходе
