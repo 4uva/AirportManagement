90c4a4a3fe49029c493ee2f8b3260745e01e6a36
4uva <41539104+4uva@users.noreply.github.com>
2019-01-03 01:43:41 +0200

    Added single Airport output routine. Added Airport list routine, based on the former one.

--- AirportManagement/Presentation/DataonScreen.cs
+++ AirportManagement/Presentation/DataonScreen.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-
+using AirportManagement.Data;
 namespace AirportManagement.Presentation
 {
     class DataonScreen
     {
-
         public DataonScreen()
         {
-            DataonScreen(Output)//invoke Airports on screen
         }
-
-        public static void DataOutput
+        
+        public  void DataOutput(List<Airport> Airports)
         {
+           foreach  ( Airport value in Airports)// in every type element value in variable 
+           { 
+               Output(value);// 
+           }
         }
 
+        public void Output(Airport airport)//output only 1 Airport
+        {
+            var a = airport.Location;
+            var b = a.Name;//можно закомментировать
+            Console.WriteLine(b);//Console.WriteLine(airport.Location.Name) or `a.Name`,
+        }
     }
 }
