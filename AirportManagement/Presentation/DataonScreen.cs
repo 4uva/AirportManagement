70459e43ac1215f6079d56b8fb1d1b0bfcf62d94
4uva <41539104+4uva@users.noreply.github.com>
2019-01-03 17:22:43 +0200

    Added header to airport list via a  new  variable invocation

--- AirportManagement/Presentation/DataonScreen.cs
+++ AirportManagement/Presentation/DataonScreen.cs
@@ -8,20 +8,26 @@ namespace AirportManagement.Presentation
         public DataonScreen()
         {
         }
-        
+   
         public  void DataOutput(List<Airport> Airports)
         {
-           foreach  ( Airport value in Airports)// in every type element value in variable 
-           { 
+            string Header = "Airports";
+            Console.WriteLine(Header);
+
+            foreach  ( Airport value in Airports)// in every type element value in variable 
+            { 
                Output(value);// 
-           }
+            }
         }
 
-        public void Output(Airport airport)//output only 1 Airport
-        {
-            var a = airport.Location;
-            var b = a.Name;//можно закомментировать
-            Console.WriteLine(b);//Console.WriteLine(airport.Location.Name) or `a.Name`,
-        }
+        //void HeaderPresentation()
+        //{
+        //}
+         void Output(Airport airport)//output only 1 Airport
+         {  
+             var a = airport.Location;
+             var b = a.Name;//можно закомментировать
+             Console.WriteLine(b);//Console.WriteLine(airport.Location.Name) or `a.Name`,
+         }
     }
 }
