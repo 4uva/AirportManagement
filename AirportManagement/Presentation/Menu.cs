532e5a3bb0284388d8fe6c4bbc3457a788868d6b
4uva <41539104+4uva@users.noreply.github.com>
2019-01-12 01:57:42 +0200

    Formatted Menu class, gathered inner functions into a single Run function

--- AirportManagement/Presentation/Menu.cs
+++ AirportManagement/Presentation/Menu.cs
@@ -10,81 +10,85 @@ namespace AirportManagement.Presentation
               "Hi! This is admin panel. You could  " +
               "add airport - press a, " +
               " update - b, " +
-              "delete - d, " + "to cancel operation -c " + 
+              "delete - d, " + "to cancel operation -c " +
               "Please make your choice"
             );
         }
 
         public UserChoice IdentifyInput(string UserInput)//вот откуда эта 
-                                                  //функция берет значение UserInput
-     
-        { const string a = "a";
+                                                         //функция берет значение UserInput
+        {
+            const string a = "a";
             const string b = "b";
             const string c = "c";
             const string d = "d";
-            
 
             switch (UserInput)
             {
                 case a:
-                 return UserChoice.Add;
-                    
+                    return UserChoice.Add;
+
                 case b:
-                 return UserChoice.Update;
-                    
+                    return UserChoice.Update;
+
                 case c:
-                  return UserChoice.Delete ;
-                   
+                    return UserChoice.Delete;
+
                 case d:
-                return   UserChoice.Cancel ;
-                    
+                    return UserChoice.Cancel;
+
                 default:
-                  return UserChoice.Irrelevant ;
-                  
+                    return UserChoice.Irrelevant;
             }
         }
-              public void DisplayInputPrompt (UserChoice userChoice)
 
-              //если юзер хочет добавить аэропорт, 
-              //то нам нужно запросить имя аэропорта, 
-              //прочитать его и добавить
-                 {
-                     if (  userChoice == UserChoice.Add)
-                {
-                 Console.WriteLine ("Please add an Airport");
+        public void DisplayInputPrompt(UserChoice userChoice)
+        //если юзер хочет добавить аэропорт, 
+        //то нам нужно запросить имя аэропорта, 
+        //прочитать его и добавить
+        {
+            if (userChoice == UserChoice.Add)
+            {
+                Console.WriteLine("Please add an Airport");
 
-                }  
-                if ( userChoice == UserChoice.Update)
-                {
-                 Console.WriteLine ("Please update an Airport");
+            }
+            if (userChoice == UserChoice.Update)
+            {
+                Console.WriteLine("Please update an Airport");
 
-                }  
-                if ( userChoice == UserChoice.Update)
-                {
-                 Console.WriteLine ("Please update an Airport");
+            }
+            if (userChoice == UserChoice.Update)
+            {
+                Console.WriteLine("Please update an Airport");
 
-                }  
-                if ( userChoice == UserChoice.Cancel)
-                {
-                 Console.WriteLine ("Please choose other option");
+            }
+            if (userChoice == UserChoice.Cancel)
+            {
+                Console.WriteLine("Please choose other option");
 
-                }  
-                if ( userChoice == UserChoice.Delete)
-                {
-                 Console.WriteLine ("Please delete an Airport");
+            }
+            if (userChoice == UserChoice.Delete)
+            {
+                Console.WriteLine("Please delete an Airport");
 
-                }  
-                if ( userChoice == UserChoice.Irrelevant)
-                {
-                 Console.WriteLine ("Please try again");
+            }
+            if (userChoice == UserChoice.Irrelevant)
+            {
+                Console.WriteLine("Please try again");
+            }
+             ;
+            //eсли юзер хочет удалить аэропорт, то нам нужно
+            // вывести список, запросить номер или имя, и удалить его
+            //а если хочет переименовать, то вывести список, запросить 
+            //номер или имя, запросить новое имя, и присвоить   
+        }
 
-                }  
-
-               //eсли юзер хочет удалить аэропорт, то нам нужно
-               // вывести список, запросить номер или имя, и удалить его
-               //а если хочет переименовать, то вывести список, запросить 
-               //номер или имя, запросить новое имя, и присвоить   
- 
+        public UserChoice Run()
+        {
+            DisplayPrompt();
+            string UserInput = Console.ReadLine();
+            UserChoice userChoice = IdentifyInput(UserInput);
+            return userChoice;
         }
     }
 }
--- AirportManagement/Program.cs
+++ AirportManagement/Program.cs
@@ -16,25 +16,25 @@ namespace AirportManagement
             AirportPresentation airportPresentation = new AirportPresentation();
             //airportPresentation.Output(all.Airports);
             Menu menu = new Menu();
-            menu.DisplayPrompt();
-            string UserInput = Console.ReadLine();
-            UserChoice userChoice =menu.IdentifyInput(UserInput);
-            menu.DisplayInputPrompt (userChoice);
+
+            menu.Run();
+            //UserChoice userChoice =
+            menu.DisplayInputPrompt(userChoice);
             //userchoice.тут нужно вызвать функцию  у объекта
-           
-         //4) программа видит, что юзер выбрал добавить,
+
+            //4) программа видит, что юзер выбрал добавить,
             // и выдаёт приглашение ввести аэропорт (где это будет в коде?)`
             // AirPortPresentation layor нужна ли новая функция, или
             // мы можем вложить это приглашение в функцию DisplayPrompt()- menu class`
-           // airportPresentation.Output();
+            // airportPresentation.Output();
             //all.AddAirport (airportPresentation);
-            
+
             //сли юзер хочет добавить аэропорт, 
             //то нам нужно запросить имя аэропорта, 
             //прочитать его и добавить
             //если юзер хочет удалить аэропорт, 
             //то нам нужно вывести список, запросить
-             //номер или имя, и удалить его
+            //номер или имя, и удалить его
             //а если хочет переименовать, 
             //то вывести список, запросить номер или имя, запросить новое имя, и присвоить
         }
