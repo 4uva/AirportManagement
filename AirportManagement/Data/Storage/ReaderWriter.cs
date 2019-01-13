87acbe29c339e89bb909ebc18765cd1e38ed7adc
4uva <41539104+4uva@users.noreply.github.com>
2019-01-14 01:09:15 +0200

    create a new subfolder in Airport.Data namespace as a preparation to serialization.

---
+++ AirportManagement/Data/Storage/ReaderWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirportManagement.Data.Storage
+{
+    class ReaderWriter
+    {
+
+        public static All Read()
+        { return new All(); }
+        public static void Write (All all )
+        {  }
+
+    }
+}
