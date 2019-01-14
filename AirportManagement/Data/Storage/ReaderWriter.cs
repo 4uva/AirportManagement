266f8b1f3e579337001d5b8552a3ab012228d637
4uva <4uva@portaone.com>
2019-01-14 13:58:41 +0200

    Implemented ReaderWriter.Write, added Newtonsoft.JSON for serialization

--- AirportManagement/Data/Storage/ReaderWriter.cs
+++ AirportManagement/Data/Storage/ReaderWriter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace AirportManagement.Data.Storage
 {
@@ -10,7 +12,27 @@ namespace AirportManagement.Data.Storage
         public static All Read()
         { return new All(); }
         public static void Write (All all )
-        {  }
+        { 
+         
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+            using (StreamWriter sw = new StreamWriter("all.json"))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+             serializer.Serialize(writer, all); 
+            //    // {"ExpiryDate":new Date(1230375600000),"Price":0}this is a method body,i cant make a semanic conection 
+            //}
+
+
+
+
+
+
+
+
+
+         }
 
     }
+}
 }
