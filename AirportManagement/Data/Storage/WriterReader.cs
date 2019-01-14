1a18a06acd9611513838ac6819ff4476fb1cdebf
4uva <4uva@portaone.com>
2019-01-14 14:54:22 +0200

    Implemented writing and renamed class as WriterReader

--- AirportManagement/Data/Storage/WriterReader.cs
+++ AirportManagement/Data/Storage/WriterReader.cs
@@ -6,33 +6,29 @@ using Newtonsoft.Json;
 
 namespace AirportManagement.Data.Storage
 {
-    class ReaderWriter
+    class WriterReader
     {
 
-        public static All Read()
-        { return new All(); }
         public static void Write (All all )
         { 
-         
             JsonSerializer serializer = new JsonSerializer();
             serializer.NullValueHandling = NullValueHandling.Ignore;
             using (StreamWriter sw = new StreamWriter("all.json"))
             using (JsonWriter writer = new JsonTextWriter(sw))
             {
-             serializer.Serialize(writer, all); 
-            //    // {"ExpiryDate":new Date(1230375600000),"Price":0}this is a method body,i cant make a semanic conection 
-            //}
+                serializer.Serialize(writer, all); 
+            }
+        }
 
-
-
-
-
-
-
-
-
-         }
-
+        public static All Read()
+        { 
+            // deserialize JSON directly from a file
+            using (StreamReader file = File.OpenText("all.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                All all = (All)serializer.Deserialize(file, typeof(All));
+                return all;
+            } 
+        }
     }
-}
 }
