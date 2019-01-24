18d82d2a6b45859c82317a632c714bf6bb3d6582
4uva <41539104+4uva@users.noreply.github.com>
2019-01-24 02:04:59 +0200

    Refactored RunStep into individual functions which process a single user action. Added correct return values to RunStep function.

--- AirportManagement/BusinessLogic/MainLogic.cs
+++ AirportManagement/BusinessLogic/MainLogic.cs
@@ -11,73 +11,68 @@ namespace AirportManagement.BusinessLogic
         {
             this.all = all;
         }
-       
-        public StepResult RunStep()
+
+        void ProcessAdd()
         {
-            
-            UserChoice userchoice = menu.Run();
-            //это дублирующая логика 
-            //только для слоя презентации 
-            if (userchoice == UserChoice.Add)
-            {
-                Console.WriteLine("Please add an Airport");
-                string userInput = Console.ReadLine();
-                all.AddAirport(userInput);
-            }
+            Console.WriteLine("Please add an Airport");
+            string userInput = Console.ReadLine();
+            all.AddAirport(userInput);
+        }
 
-            //если юзер выбрал удалить
-            if (userchoice == UserChoice.Delete)
+        void ProcessDelete()
+        {
+            //просим ввести аэропорт частично
+            Console.WriteLine("Enter part of airport's name");
+            //читаем ввод
+            string airportPartialName = Console.ReadLine();
+            //создаем список аэропортов на основе вызова функции фильтрации
+            List<Airport> filteredAirports =
+                all.GetFilteredByPartialLocationAirports(airportPartialName);
+
+            //если длина списка больше 1
+            if (filteredAirports.Count > 1)
             {
-                //просим ввести аэропорт частично
-                Console.WriteLine("Enter part of airport's name");
-                //читаем ввод
-                string airportPartialName = Console.ReadLine();
-                //создаем список аэропортов на основе вызова функции фильтрации
-                List<Airport> filteredAirports =
-                    all.GetFilteredByPartialLocationAirports(airportPartialName);
-                //если длина списка больше 1
-                if (filteredAirports.Count > 1)
+                //если юзер выводим сообщение нашли такие аэропорты
+                Console.WriteLine("Found airports:");
+                //обращаемся к слою презентация и выводим аэропорты
+                airportPresentation.Output(filteredAirports);
+                //просим ввести больше букв или номер аэропорта в списке
+                Console.WriteLine("Enter a bigger part of airport's name " +
+                    "or number in the list above");
+
+                //читаем ввод юзера и записываем переменную строку
+                string indexOrSubstring = Console.ReadLine();
+
+                //проверяем или введенная строка не является число,
+                //переменная выхолит со значением, а вхдит не с чем
+                if (int.TryParse(indexOrSubstring, out int index))
+                //проверили строка или число
                 {
-                    //если юзер выводим сообщение нашли такие аэропорты
-                    Console.WriteLine("Found airports:");
-                    //обращаемся к слою презентация и выводим аэропорты
-                    airportPresentation.Output(filteredAirports);
-                    //просим ввести больше букв или номер аэропорта в списке
-                    Console.WriteLine("Enter a bigger part of airport's name " +
-                        "or number in the list above");
-
-                    //читаем ввод юзера и записываем переменную строку
-                    string indexOrSubstring = Console.ReadLine();
-                    //проверяем или введенная строка не является число,
-                    //переменная выхолит со значением, а вхдит не с чем
-                    if (int.TryParse(indexOrSubstring, out int index))
-                    //проверили строка или число
+                    //проверяем, чтоб индекс был в списке
+                    if (index < filteredAirports.Count && index >= 0)
                     {
-                        //проверяем, чтоб индекс был в списке
-                        if (index < filteredAirports.Count && index >= 0)
-                        {
-                            Airport userSelectedAirport = filteredAirports[index];
-                            Console.WriteLine("Identified airport, " +
-                            userSelectedAirport.Location.Name +
-                                "enter d for delete or enter for cancel");
-                            //нашлиns  аропорт просим юзера /
-                            //удалилить или отменить операцию удаления
-                            string deletion = Console.ReadLine();
+                        Airport userSelectedAirport = filteredAirports[index];
+                        Console.WriteLine("Identified airport, " +
+                        userSelectedAirport.Location.Name +
+                            "enter d for delete or enter for cancel");
+                        //нашлиns  аропорт просим юзера /
+                        //удалилить или отменить операцию удаления
+                        string deletion = Console.ReadLine();
 
-                            // if (userchoice == UserChoice.Delete)//если юзер выбрал удалить
-                            if (deletion == "d")
-                            {
-                                all.DeleteAirport(userSelectedAirport);
-                                Console.WriteLine(" Selected airport was sucessfully deleted");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Deletion canceled");
-                            }
+                        // if (userchoice == UserChoice.Delete)//если юзер выбрал удалить
+                        if (deletion == "d")
+                        {
+                            all.DeleteAirport(userSelectedAirport);
+                            Console.WriteLine(" Selected airport was sucessfully deleted");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Deletion canceled");
                         }
                     }
                 }
             }
+
             // 3 варианта:
             // 1) много аэропортов, выводим их все, теперь запрашиваем индекс или подстроку.
             //    если получили индекс:
@@ -90,19 +85,31 @@ namespace AirportManagement.BusinessLogic
             // 2) ноль аэропортов. запрашиваем другую подстроку, фильтруем по ней
             //    и снова три варианта
             // 3) один аэропорт — он найден
+        }
+
+        void ProcessIrrelevant()
+        {
+            Console.WriteLine("Wrong choice. Please try again");
+        }
+
+        public StepResult RunStep()
+        {
+            UserChoice userchoice = menu.Run();
+            //это дублирующая логика только для слоя презентации 
+            if (userchoice == UserChoice.Add)
+                ProcessAdd();
+
+            //если юзер выбрал удалить
+            if (userchoice == UserChoice.Delete)
+                ProcessDelete();
 
             if (userchoice == UserChoice.Irrelevant)
-            {
-                //4) программа видит, что юзер выбрал добавить,
-                // и выдаёт приглашение ввести аэропорт
-                Console.WriteLine("Wrong choice. Please try again");
-            }
+                ProcessIrrelevant();
 
             if (userchoice == UserChoice.Cancel)
-            {
-                
-                StepResult.Exit;
-            }
+                return StepResult.Exit;
+
+            return StepResult.Continue;
         }
 
         //это поле, которое инициализируется ссылкой на экземпляр объекта
