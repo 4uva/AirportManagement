419fe0fe71f519f0e078988d2e1fcf9f73317808
4uva <41539104+4uva@users.noreply.github.com>
2019-01-29 01:20:41 +0200

    AirportOnScreen: adding indexed output WIP

--- AirportManagement/Presentation/AirportOnScreen.cs
+++ AirportManagement/Presentation/AirportOnScreen.cs
@@ -23,7 +23,27 @@ namespace AirportManagement.Presentation
             var b = a.Name;//можно закомментировать
             Console.WriteLine(b);//Console.WriteLine(airport.Location.Name) or `a.Name`,
         }
+        public void OutputIndexed(List<Airport> Airports)
+        {
+            {
+                for (int i = 0; i < Airports.Count - 1; i++)// in every type element value in variable 
+                {
+                    int[] index = Airports.Count - 1;//x-y+1
 
+                    /* for (int i = 0; i < 991; i++)
+                     {
+                         int c = i + 10;
+                         a[i] = c;
+                     }*/
+                    /* for (int c = 10; c <= 1000; c++)
+                    {
+                        int i = c - 10;
+                        a[i] = c;
+                    }*/
+                }
+            }
+
+        }
         //public string AirportDatabaseCreation(string UserInput2)
         //{
         //    Console.WriteLine("Please add airport name");
