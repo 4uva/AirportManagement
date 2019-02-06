a6fbf7b9bb7477e1a11b52e7802a5854aed02db2
4uva <41539104+4uva@users.noreply.github.com>
2019-02-06 23:56:02 +0200

    Moved time zone from Airport to Location

--- AirportManagement.Data/Airport.cs
+++ AirportManagement.Data/Airport.cs
@@ -1,24 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AirportManagement.Data
+﻿namespace AirportManagement.Data
 {
     public class Airport //чтобы тут не было, поля, 
                          //переменные свойства -это определение класса
-    
     {
         //название аэропорта прибытия, город аэропорта
         //прибытия, погода в аэропорту прибытия, часовой
         // пояс аэропорта прибытия — все эти данные 
         //можно считать частью прибытия
 
-        //TODO: добавить конструктор, устанавливающий Location и
-        //LocalTimeZone
-        public Location Location{ get; set; }
-        public Weather Weather{ get; set; }
-        public TimeZoneInfo LocalTimeZone{ get; set; }
+        //TODO: добавить конструктор, устанавливающий Location
+        public Location Location { get; set; }
+        public Weather Weather { get; set; }
     }
 }
--- AirportManagement.Data/Location.cs
+++ AirportManagement.Data/Location.cs
@@ -1,7 +1,10 @@
-﻿namespace AirportManagement.Data
+﻿using System;
+
+namespace AirportManagement.Data
 {
     public class Location
     {
         public string Name { get; set; }
+        public TimeZoneInfo LocalTimeZone { get; set; }
     }
 }
