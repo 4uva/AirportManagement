4f022a7e71d8120c4c618d019e037569567d8091
4uva <41539104+4uva@users.noreply.github.com>
2019-02-09 01:10:47 +0200

    Database part split off the All. Using Eager loading for the navigational properties now

--- AirportManagement.Data/All.cs
+++ AirportManagement.Data/All.cs
@@ -8,61 +8,8 @@ using Microsoft.EntityFrameworkCore;
 namespace AirportManagement.Data
 {
     //добавить списки, аэропортов, полётов, и авиакомпаний
-    public class All : DbContext//унаследовали из библ.класса
-    {//todo where is datasset created
-        public DbSet<Airport> Airports { get; set; }//List updated to Dataset
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {//to do read info about OnConfiguring
-            // TODO: move the connection string to the configuration file
-            const string dbName = "AirportsDb";//что это за константа
-            optionsBuilder.UseSqlServer(
-                $@"Server=(localdb)\mssqllocaldb;Database={dbName};Trusted_Connection=True;");
-            optionsBuilder.EnableSensitiveDataLogging();
-            base.OnConfiguring(optionsBuilder);
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // seeding Airports table
-            // https://docs.microsoft.com/en-us/ef/core/modeling/data-seeding
-            
-            var seededAirports = new[]
-            {
-                CreateSeedAirportWithLocation("Florence", -1, -1),
-                CreateSeedAirportWithLocation("Elabuga", -2, -2),
-                CreateSeedAirportWithLocation("Gatwick", -3, -3),
-                CreateSeedAirportWithLocation("Innsbruk", -4, -4),
-                CreateSeedAirportWithLocation("Budapest", -5, -5),
-                CreateSeedAirportWithLocation("Bucharest", -6, -6),
-                CreateSeedAirportWithLocation("Buchara", -7, -7)
-            };
-
-            // к объекту modelBuilder создаем базу и вызываем метод HasData 
-            modelBuilder.Entity<Location>().HasData(seededAirports.Select(a => a.Location));
-            // workaround for bug https://github.com/aspnet/EntityFrameworkCore/issues/10000
-            foreach (var a in seededAirports)
-                a.Location = null;
-            modelBuilder.Entity<Airport>().HasData(seededAirports);
-
-            base.OnModelCreating(modelBuilder);
-        }
-        
-        Airport CreateSeedAirportWithLocation(string locationName, int airportId, int locationId)
-        {
-            return new Airport()
-            {
-                Id = airportId,// Airport has its own primary key
-                LocationId = locationId,//Location id is a foreign key for Airport
-                Location = new Location()
-                {
-                    Id = locationId,//primary key for Location
-                    Name = locationName
-                }
-            };
-        }
-        // utility functions
-
+    public class All : IDisposable
+    {
         public Airport AddAirport(string locationName)
         // создали публичную функцию добавить аэропорт , котррая
         //возращает аэропорт и на вход получает имя аэропорта
@@ -70,7 +17,7 @@ namespace AirportManagement.Data
             var a = CreateAirport(locationName);
             //создали переменную и присвоили ей значение вызова 
             //функции с параметром локация аэропорта
-            Airports.Add(a);//добавили экземпляр аэропорта 
+            dbContext.Airports.Add(a);//добавили экземпляр аэропорта 
             //со значением фактического параметра а
             return a;
             //вернули значение фактического параметра
@@ -78,7 +25,7 @@ namespace AirportManagement.Data
 
         public void DeleteAirport(Airport airport)
         {   
-            Airports.Remove(airport);
+            dbContext.Airports.Remove(airport);
         }
 
         Airport CreateAirport(string locationName)
@@ -96,26 +43,31 @@ namespace AirportManagement.Data
             // чтоб эти данные а можно было записать в функции
         }
 
-        //поле r
-        //All All2 { get; set; }  свойство
-        public void Create()
-        {
-            AddAirport("Florence");//вызвали экземпляр списка
-            AddAirport("Elabuga");
-            AddAirport("Gatwick");
-            AddAirport("Innsbruk");
-            AddAirport("Budapest");
-            AddAirport("Bucharest");
-            AddAirport("Buchara");
-        }
-
         public List<Airport> GetFilteredByPartialLocationAirports(string partialLocation)
-        {
+        {//todo WHAT IS LINQ
             return Airports
                 .Where(a => a.Location.Name.Contains(partialLocation, StringComparison.InvariantCultureIgnoreCase))
                 .OrderBy(a => a.Location.Name)
                 .ToList();
         }
+
+        public void Dispose()
+        {
+            dbContext.SaveChanges();
+            dbContext.Dispose();
+        }
+
+        public IEnumerable<Airport> Airports
+        {
+            get
+            {
+                return dbContext.Airports
+                                .Include(a => a.Location)
+                                .Include(a => a.Weather);
+            }
+        }
+
+        EF.AirportDbContext dbContext = new EF.AirportDbContext();
     }
 }
 
---
+++ AirportManagement.Data/EF/AirportDbContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace AirportManagement.Data.EF
+{
+    class AirportDbContext : DbContext//унаследовали из библ.класса
+    {
+        //todo where is datasset created
+        public DbSet<Airport> Airports { get; set; }
+
+        // https://docs.microsoft.com/en-us/ef/core/miscellaneous/configuring-dbcontext
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {//to do read info about OnConfiguring
+            // TODO: move the connection string to the configuration file
+            const string dbName = "AirportsDb";//что это за константа
+            optionsBuilder.UseSqlServer(
+                $@"Server=(localdb)\mssqllocaldb;Database={dbName};Trusted_Connection=True;");//расшифровка строк подключения на будущее Trusted_Connection=True;
+            optionsBuilder.EnableSensitiveDataLogging();//switch on migration for debugging
+            base.OnConfiguring(optionsBuilder);//methode invocation from parental c
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // seeding Airports table
+            // https://docs.microsoft.com/en-us/ef/core/modeling/data-seeding
+
+            var seededAirports = new[]
+            {
+                CreateSeedAirportWithLocation("Florence", -1, -1),
+                CreateSeedAirportWithLocation("Elabuga", -2, -2),
+                CreateSeedAirportWithLocation("Gatwick", -3, -3),
+                CreateSeedAirportWithLocation("Innsbruk", -4, -4),
+                CreateSeedAirportWithLocation("Budapest", -5, -5),
+                CreateSeedAirportWithLocation("Bucharest", -6, -6),
+                CreateSeedAirportWithLocation("Buchara", -7, -7)
+            };
+
+            // к объекту modelBuilder создаем базу и вызываем метод HasData
+            // todo WHAT IS GENERIC
+            modelBuilder.Entity<Location>().HasData(seededAirports.Select(a => a.Location));
+            //из каждого аэропорта берём `Location`, и делаем из них 
+            //новый список~ новую коллекцию
+            //и то, что получилось, передаём аргументом
+            //в HasDataто есть все локации
+            // workaround for bug https://github.com/aspnet/EntityFrameworkCore/issues/10000
+            foreach (var a in seededAirports)
+                a.Location = null;
+            modelBuilder.Entity<Airport>().HasData(seededAirports);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        Airport CreateSeedAirportWithLocation(string locationName, int airportId, int locationId)
+        {
+            return new Airport()
+            {
+                Id = airportId,// Airport has its own primary key
+                LocationId = locationId,//Location id is a foreign key for Airport
+                Location = new Location()
+                {
+                    Id = locationId,//primary key for Location
+                    Name = locationName
+                }
+            };
+        }
+    }
+}
