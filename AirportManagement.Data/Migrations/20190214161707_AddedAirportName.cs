7ff00e26689d3a6e7c2b5b9ce4803ce8c9eb75a9
4uva <41539104+4uva@users.noreply.github.com>
2019-02-14 18:41:13 +0200

    Added new migration AddedAirportName in EF because of added property (part 2).

---
+++ AirportManagement.Data/Migrations/20190214161707_AddedAirportName.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirportManagement.Data.Migrations
+{
+    public partial class AddedAirportName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Airports",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Airports",
+                keyColumn: "Id",
+                keyValue: -7,
+                column: "Name",
+                value: "Buchara");
+
+            migrationBuilder.UpdateData(
+                table: "Airports",
+                keyColumn: "Id",
+                keyValue: -6,
+                column: "Name",
+                value: "Henri Coandă");
+
+            migrationBuilder.UpdateData(
+                table: "Airports",
+                keyColumn: "Id",
+                keyValue: -5,
+                column: "Name",
+                value: "Ferenc Liszt");
+
+            migrationBuilder.UpdateData(
+                table: "Airports",
+                keyColumn: "Id",
+                keyValue: -4,
+                column: "Name",
+                value: "Kranebitten");
+
+            migrationBuilder.UpdateData(
+                table: "Airports",
+                keyColumn: "Id",
+                keyValue: -3,
+                column: "Name",
+                value: "Gatwick");
+
+            migrationBuilder.UpdateData(
+                table: "Airports",
+                keyColumn: "Id",
+                keyValue: -2,
+                column: "Name",
+                value: "Kazan");
+
+            migrationBuilder.UpdateData(
+                table: "Airports",
+                keyColumn: "Id",
+                keyValue: -1,
+                column: "Name",
+                value: "Amerigo Vespuchi");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Airports");
+        }
+    }
+}
