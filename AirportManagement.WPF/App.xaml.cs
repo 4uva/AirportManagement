e181d7b92e86908e8585979477727d1278a647c9
4uva <41539104+4uva@users.noreply.github.com>
2019-02-24 19:16:22 +0200

    added some comments for learning purpose

--- AirportManagement.WPF/App.xaml.cs
+++ AirportManagement.WPF/App.xaml.cs
@@ -12,13 +12,20 @@ namespace AirportManagement.WPF
 {
     public partial class App : Application
     {
+    //    аргументы protected override void OnStartup(StartupEventArgs e)  используем
+    //    потому что у перекрытой функции должна быть в точности такая же сигнатура,
+    //    как и у перекрываемой(edited), а раз у перекрываемой функции передаётся
+    //   параметр такого типа, то и нам нужно использовать его не обязательн
+    //    но обязательно передать базовой функции
+
+
         protected override void OnStartup(StartupEventArgs e)//поясни Raises the Startup event.
         {
             base.OnStartup(e);
-            var repository = new Repository(); // TODO: не грузить базу в UI-потоке
-            var listViewModel = new AirportListViewModel(repository);//откуда взялся этот тип
-            var window = new MainWindow() { DataContext = listViewModel }; //поясни всю строку MainWindow is automatically set with a reference to the first Window object to be instantiated in the AppDomain.
-            window.Show();
+            var repository = new Repository(); // TODO: не грузить базу в UI-потоке мы создаём репозиторий
+            var listViewModel = new AirportListViewModel(repository);//создаём ViewModel списка аэропортовr 2
+            var window = new MainWindow() { DataContext = listViewModel }; //создаём главное окно, и прикрепляем к нему ViewModel
+            window.Show();//говорим окну, «покажись!»
         }
     }
 }
