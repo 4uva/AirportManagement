8875c745859f2eac353c534da57df38a1469836a
4uva <41539104+4uva@users.noreply.github.com>
2019-02-24 19:13:29 +0200

    added properties to class for usage in entire solution

--- AirportManagement.Data/Passenger.cs
+++ AirportManagement.Data/Passenger.cs
@@ -8,5 +8,14 @@ namespace AirportManagement.Data
 {
     class Passenger
     {
+        string PassengerFamilyName { get; set; }
+        string PassengerName { get; set; }
+        int PassengerAge { get; set; }
+        Passenger Tariff { get; set; }
+        Airport PassengerDeparturePoint { get; set; }
+        Airport PassengerArrivalPoint { get; set; }
+        public int Id { get; set; }
+        public int FlightId { get; set; }
+        private PassengerStatus current = PassengerStatus.Checkin;
     }
 }
