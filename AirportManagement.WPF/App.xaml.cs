fa83a4268ae922f7f1e7723644dac1ac944bbc9e
4uva <41539104+4uva@users.noreply.github.com>
2019-02-28 01:35:43 +0200

    Added some explanatory comments

--- AirportManagement.WPF/App.xaml.cs
+++ AirportManagement.WPF/App.xaml.cs
@@ -25,6 +25,18 @@ namespace AirportManagement.WPF
             var repository = new Repository(); // TODO: не грузить базу в UI-потоке мы создаём репозиторий
             var listViewModel = new AirportListViewModel(repository);//создаём ViewModel списка аэропортовr 2
             var window = new MainWindow() { DataContext = listViewModel }; //создаём главное окно, и прикрепляем к нему ViewModel
+            //если мы хотим, чтобы наш View - элемент отображал
+            // (то есть, являлся _представлением_) какого - то VM - объекта
+            //  (который представляет собой _контент_), то мы устанавливаем 
+            ////DataContext'ом этого View-элемента (контрола) этот самый VM-объект
+            //new Airport() аналог { DataContext = listViewModel  совершенно привычный инициализатор объекта:
+            //{
+            //    Name = airportName,
+            //    Location = new Location
+            //    {
+            //        Name = locationName
+            //    }
+            //};
             window.Show();//говорим окну, «покажись!»
         }
     }
