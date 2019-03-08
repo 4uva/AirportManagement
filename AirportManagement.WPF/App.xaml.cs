de53567ae3d243be6fe8179bf119cee28c169e83
4uva <41539104+4uva@users.noreply.github.com>
2019-03-08 17:57:10 +0200

    Saving the database on exit

--- AirportManagement.WPF/App.xaml.cs
+++ AirportManagement.WPF/App.xaml.cs
@@ -12,17 +12,12 @@ namespace AirportManagement.WPF
 {
     public partial class App : Application
     {
-    //    аргументы protected override void OnStartup(StartupEventArgs e)  используем
-    //    потому что у перекрытой функции должна быть в точности такая же сигнатура,
-    //    как и у перекрываемой(edited), а раз у перекрываемой функции передаётся
-    //   параметр такого типа, то и нам нужно использовать его не обязательн
-    //    но обязательно передать базовой функции
-
+        Repository repository = new Repository();
 
         protected override void OnStartup(StartupEventArgs e)//поясни Raises the Startup event.
         {
             base.OnStartup(e);
-            var repository = new Repository(); // TODO: не грузить базу в UI-потоке мы создаём репозиторий
+
             var listViewModel = new AirportListViewModel(repository);//создаём ViewModel списка аэропортовr 2
             var window = new MainWindow() { DataContext = listViewModel }; //создаём главное окно, и прикрепляем к нему ViewModel
             //если мы хотим, чтобы наш View - элемент отображал
@@ -38,6 +33,12 @@ namespace AirportManagement.WPF
             //    }
             //};
             window.Show();//говорим окну, «покажись!»
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            repository.Dispose();
+            base.OnExit(e);
         }
     }
 }
