2355b7d3f1f7d1d52400050f0243e593f027ad9b
4uva <41539104+4uva@users.noreply.github.com>
2019-03-13 00:55:14 +0200

    Added new class in VM to set up Adding airport WIP

---
+++ AirportManagement.WPF/VM/AddingAirportViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportManagement.WPF.VM
+{
+    class AddingAirportViewModel : ViewModel, INotifyPropertyChanged
+    {
+        public string AirportName
+        {
+            get => airportName;
+            set => Set(ref airportName, value);//присваивание свойству есть вызов сеттера
+        }
+        string airportName;
+        public string AirportLocation
+       
+        {
+            get => airportLocation;
+            set => Set(ref airportLocation, value);//присваивание свойству есть вызов сеттера
+        }
+
+        string airportLocation;
+    }
+}
