7451070e6d8e994238cc407d4ac0fbd35dae19cd
4uva <41539104+4uva@users.noreply.github.com>
2019-03-20 00:41:48 +0200

    Moving common adding entity logic to a common generic base class

--- AirportManagement.WPF/VM/AddingAirportViewModel.cs
+++ AirportManagement.WPF/VM/AddingAirportViewModel.cs
@@ -4,24 +4,39 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AirportManagement.Data;
 
 namespace AirportManagement.WPF.VM
 {
-    class AddingAirportViewModel : ViewModel, INotifyPropertyChanged
+    class AddingAirportViewModel : AddingGeneric<Airport>
     {
+        public AddingAirportViewModel()
+        {
+        }
+
+        protected override Airport Save()
+        {
+            throw new NotImplementedException();
+            // TODO: crteate through repository
+        }
+
+        protected override bool CanSave()
+        {
+            return !string.IsNullOrWhiteSpace(AirportName) && !string.IsNullOrWhiteSpace(AirportLocation);
+        }
+
         public string AirportName
         {
             get => airportName;
             set => Set(ref airportName, value);//присваивание свойству есть вызов сеттера
         }
         string airportName;
-        public string AirportLocation
-       
+
+        public string AirportLocation       
         {
             get => airportLocation;
             set => Set(ref airportLocation, value);//присваивание свойству есть вызов сеттера
         }
-
         string airportLocation;
     }
 }
---
+++ AirportManagement.WPF/VM/AddingGeneric.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportManagement.WPF.VM
+{
+    abstract class AddingGeneric<T> : ViewModel
+    {
+        AirportListViewModel parent;
+        public AddingGeneric()
+        {
+            RelayCommand SaveCommand = new RelayCommand(SaveAndExit, CanSave);
+            RelayCommand CancelCommand = new RelayCommand(CancelAndExit, CanCancel);
+        }
+
+        void SaveAndExit()
+        {
+            T addedEntity = Save();
+            parent.AddingFinished(addedEntity); // какая сигнатура AddingFinished?
+        }
+
+        protected abstract T Save();
+        protected abstract bool CanSave();
+        bool CanCancel() => true;
+        void CancelAndExit()
+        {
+            parent.AddingFinished(null);
+        }
+
+        public RelayCommand SaveCommand { get; }
+        public RelayCommand CancelCommand { get; }
+    }
+}
